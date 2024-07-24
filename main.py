7dc2e7de7eb8e092fc48e180bf53a2ed9e8dcc7a
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-07-24 17:01:23 -0400

    Added a pause feature when user presses the space bar

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PIL.Image import fromarray
 from draw_frame import draw_frame
 from format_second import format_second
 from scale import resize
+from key_poller import KeyPoller
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -22,25 +23,32 @@ target_fps = cap.get(cv.CAP_PROP_FPS)
 target_frame_time = 1 / (target_fps + 1)
 seconds_to_take = frame_count / target_fps
 
-while True:
-    flag, frame = cap.read()
-    frame_n += 1
+with KeyPoller() as key_poller:
+    while True:
+        flag, frame = cap.read()
+        frame_n += 1
 
-    if flag:
-        frame_start = time.perf_counter()
-        img = fromarray(frame)
-        resize(img)
-        if frame_n > 1 and img.size != last_size:
-            os.system('cls' if os.name == 'nt' else 'clear')
-        last_size = img.size
-        frame = draw_frame(img)
-        frame_time = time.perf_counter() - frame_start
-        if frame_time < target_frame_time:
-            time.sleep(target_frame_time - frame_time)
-        fps = (1 / (time.perf_counter() - frame_start))
-        print(
-            f'\033[0;0HFrame {frame_n}/{frame_count}, {img.width}x{img.height}, {len(frame)} chars, {fps:.1f}fps, {format_second(frame_n / target_fps)}/{format_second(seconds_to_take)}',
-            frame, sep='\n', end='', flush=True
-        )
-    else:
-        break
+        if flag:
+            frame_start = time.perf_counter()
+            char = key_poller.poll()
+            if char == ' ':
+                while True:
+                    char = key_poller.poll()
+                    if char == ' ':
+                        break
+            img = fromarray(frame)
+            resize(img)
+            if frame_n > 1 and img.size != last_size:
+                os.system('cls' if os.name == 'nt' else 'clear')
+            last_size = img.size
+            frame = draw_frame(img)
+            frame_time = time.perf_counter() - frame_start
+            if frame_time < target_frame_time:
+                time.sleep(target_frame_time - frame_time)
+            fps = (1 / (time.perf_counter() - frame_start))
+            print(
+                f'\033[0;0HFrame {frame_n}/{frame_count}, {img.width}x{img.height}, {len(frame)} chars, {fps:.1f}fps, {format_second(frame_n / target_fps)}/{format_second(seconds_to_take)}',
+                frame, sep='\n', end='', flush=True
+            )
+        else:
+            break
